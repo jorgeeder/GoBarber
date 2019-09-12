9075ee2f7b4a952f09914649dcb0b130c185b861
SudoOrange <sudoorange@gmail.com>
2019-09-12 12:43:13 -0300

    Configuring Nodemailer | Sending email to Mailtrap

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'f11f113f1ce70f',
+    pass: '7d960c7b915f5b',
+  },
+  default: {
+    from: 'Equipe Gobarber  <noreply@gobarber.com>',
+  },
+};
