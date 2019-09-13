e896490097926afe9dc1dbd32987d8bdc2e5fc0b
SudoOrange <sudoorange@gmail.com>
2019-09-13 19:49:19 -0300

    Environment Variables

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'f11f113f1ce70f',
-    pass: '7d960c7b915f5b',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe Gobarber  <noreply@gobarber.com>',
